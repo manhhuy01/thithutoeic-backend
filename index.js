bed53a78dc9e776f66e8d362a8e1dd163655e559
manhhuy01 <manhhuy0112@gmail.com>
2018-08-27 16:43:03 +0700

    deploy fe

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/url', (req, res, next)=> {
+app.get('/api/example', (req, res, next)=> {
     res.json(["a", "b","c"])
 })
+
+app.use(express.static("client/dist"))
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("server running on port 3000")
