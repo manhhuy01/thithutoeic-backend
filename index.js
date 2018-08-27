533ceb0489f32e2a43bb67d66d7adf85e249757d
manhhuy01 <manhhuy0112@gmail.com>
2018-08-27 16:06:56 +0700

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/url', (req, res, next)=> {
+    res.json(["a", "b","c"])
+})
+
+app.listen(3000, ()=> {
+    console.log("server running on port 3000")
+})
